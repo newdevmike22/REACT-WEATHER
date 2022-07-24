b67b2825e74a1ff1ee34e2f81d2074064556610b
MLDODSON <paola@dominicansoul.com>
2022-07-24 11:48:19 -0500

    Branch - 2
    Branch - 2. Adding the current weather to the app.

--- weather/src/comps/api.js
+++ weather/src/comps/api.js
@@ -8,3 +8,7 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "c8a092e28a28ff55d34a5157d10aecbb";
+
