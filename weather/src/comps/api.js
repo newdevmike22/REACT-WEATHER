935e91cca9abb493b565861ae3ec4e28497cb2bf
MLDODSON <paola@dominicansoul.com>
2022-07-27 00:20:51 -0500

    Branch - 5
    Branch - 5. Weather App Updates.

--- weather/src/comps/api.js
+++ weather/src/comps/api.js
@@ -2,7 +2,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '7643935f8fmsh355eb26d1dbd758p1af588jsna011db13e4d5',
+		'X-RapidAPI-Key': '(Your API Key Here)',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -11,5 +11,5 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "c8a092e28a28ff55d34a5157d10aecbb";
+export const WEATHER_API_KEY = "(Your API Key Here)";
 
